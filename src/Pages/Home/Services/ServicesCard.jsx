4b40dd28e47b1555a1a10e1e4fd33fdbc0e9e916
sifat26 <sifatict26@gmail.com>
2024-05-08 23:35:07 +0600

    Link ta button

--- src/Pages/Home/Services/ServicesCard.jsx
+++ src/Pages/Home/Services/ServicesCard.jsx
@@ -1,6 +1,7 @@
 import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 const ServicesCard = ({ service }) => {
-  const { title, img, price } = service;
+  const { _id,title, img, price } = service;
   return (
     <div>
       <div className="card w-96 bg-base-100 shadow-xl">
@@ -15,6 +16,7 @@ const ServicesCard = ({ service }) => {
           <h2 className="card-title">{title}</h2>
           <div className=" flex font-bold text-[#FF3811]">
           <p>Price: ${price}</p>
+        <Link to={`/checkout/${_id}`}></Link>
             <button ><FaArrowRight /></button>
           </div>
         </div>
