e695eea1a3af440130c38a62cc8e76249c8254dc
sifat26 <sifatict26@gmail.com>
2024-05-08 23:48:29 +0600

    Link updated

--- src/Pages/Home/Services/ServicesCard.jsx
+++ src/Pages/Home/Services/ServicesCard.jsx
@@ -1,23 +1,22 @@
 import { FaArrowRight } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 const ServicesCard = ({ service }) => {
-  const { _id,title, img, price } = service;
+  const { _id, title, img, price } = service;
   return (
     <div>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-5 pt-5">
-          <img
-            src={img}
-            alt="Shoes"
-            className="rounded-xl "
-          />
+          <img src={img} alt="Shoes" className="rounded-xl " />
         </figure>
         <div className="card-body ">
           <h2 className="card-title">{title}</h2>
           <div className=" flex font-bold text-[#FF3811]">
-          <p>Price: ${price}</p>
-        <Link to={`/checkout/${_id}`}></Link>
-            <button ><FaArrowRight /></button>
+            <p>Price: ${price}</p>
+            <Link to={`/checkout/${_id}`}>
+              <button>
+                <FaArrowRight />
+              </button>
+            </Link>
           </div>
         </div>
       </div>
